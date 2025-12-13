74d340ba63b99e2a81e460f6159a81f098a0ecb9
Abrham Kindie <abrhamkindie805@gmail.com>
2025-12-14 00:30:30 +0300

    fixed the server error

--- server/downloader.js
+++ server/downloader.js
@@ -118,6 +118,11 @@ function downloadVideo(url, title, customPath, format, quality, options, io, onC
         ];
     }
 
+    // Add cookies if available
+    if (hasCookies) {
+        args.push('--cookies', cookiesPath);
+    }
+
     console.log(`[Downloader] Executing yt-dlp with args: ${args.join(' ')}`);
 
     let downloadProcess;
